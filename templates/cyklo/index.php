8948ba1ac1d03e45f48d5d658e45085042de52f6
WellBloud <peter.holop@gmail.com>
2018-09-19 21:40:52 +0200

    fixed Google map

--- templates/cyklo/index.php
+++ templates/cyklo/index.php
@@ -370,7 +370,9 @@ echo $option
                 </div>
             <?php endif; ?>
             <h2>Kde nás najdete</h2>
-            <div id="map"></div>
+            <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5197.629163722493!2d16.010099410052426!3d49.355657893870166!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x470d6fa82a8afed7%3A0xedd9491075a132a4!2zQ3lrbG8gSmFuw63EjWVr!5e0!3m2!1scs!2scz!4v1537385839639"
+                width="100%" height="460" frameborder="0" style="border:0" allowfullscreen></iframe>
         </div>
     <?php endif; ?>
     <?php if ($this->countModules('contact-form')) : ?>
